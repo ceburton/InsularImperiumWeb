0addfbbcaafb60d708856e0a502084b6be490220
cliff.e.burton <cliff.e.burton@gmail.com>
2026-03-07 20:16:38 +0000

    license

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,27 +2,46 @@
 
 {
   # Which nixpkgs channel to use.
-  channel = "stable-23.11"; # or "unstable"
+  channel = "stable-23.11"; 
+
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python3
     pkgs.nodejs_20
   ];
+
   # Sets environment variables in the workspace
   env = {};
-  # Runs once when the workspace is first created
-  onCreate = {
-    npm-install = "npm install --prefix web";
+
+  # The 'idx' block is required to wrap these settings
+  idx = {
+    # Search for the extensions you want to use
+    extensions = [
+      # "vscode-extension-id"
+    ];
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs only the very first time the workspace is created
+      onCreate = {
+        # Left intentionally blank, but you could put initial setup scripts here
+      };
+      
+      # Runs EVERY time the workspace starts or is rebuilt
+      onStart = {
+        npm-install = "npm install --prefix web";
+      };
+    };
+
+    # Defines previews that can be started in the side panel
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["npm" "run" "dev" "--" "--port" "3000" "--hostname" "0.0.0.0"];
+          manager = "web";
+          cwd = "web";
+        };
+      };
+    };
   };
-  # Defines a preview that can be started in the side panel
-  previews = [{
-      command = ["npm" "run" "dev"];
-      manager = "web";
-      id = "web";
-      name = "Web";
-      icon = "web";
-      port = 3000;
-      workingDirectory = "web";
-  }];
 }
